ca8b09af902b4a834642aa38e37f4a2eed8a2bad
Albert Wen <wisdomboundless@gmail.com>
2014-09-30 13:48:32 -0400

    Create ControlBar.java

---
+++ jar/ControlBar.java
@@ -0,0 +1,18 @@
+package pizazz;
+
+import static pizazz.ECastType.*;
+
+public class ControlBar {
+	ECastType type;
+	
+	public static ControlBar[] ebars = new ControlBar[16];
+	
+	public static ControlBar air = new ControlBar(AIR);
+	public static ControlBar earth = new ControlBar(EARTH);
+	public static ControlBar fire = new ControlBar(FIRE);
+	public static ControlBar water = new ControlBar(WATER);
+	
+	public ControlBar(ECastType type) {
+		this.type = type;
+	}
+}
