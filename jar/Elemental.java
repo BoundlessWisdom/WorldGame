600117fcaa4e75ec12317cdaf95b1c349c8eb88b
Albert Wen <wisdomboundless@gmail.com>
2014-10-02 14:16:18 -0400

    Update Elemental.java

--- jar/Elemental.java
+++ jar/Elemental.java
@@ -1,36 +1,53 @@
 package jar;
 
 import static jar.ECastType.*;
-public class Elemental extends Base {
+
+public /*abstract*/ class Elemental extends Base {
 	
 	public static final Elemental[] airElementals = new Elemental[256];
 	
-	public static final Elemental wisp = new Elemental(AIR, 0, 0.1f);
-	public static final Elemental wasp = new Elemental(AIR, 127, 0.1f);
+	public static final Elemental wisp = new Elemental(AIR, 0, 0.1f, 0, 0, 0);
+	public static final Elemental wasp = new Elemental(AIR, 127, 0.1f, 0, 0, 0);
+	
 	
 	public static final Elemental[] earthElementals = new Elemental[256];
 	
-	public static final Elemental mite = new Elemental(EARTH, 0, 0.5f);
+	public static final Elemental mite = new Elemental(EARTH, 0, 0.5f, 0, 0, 0);
+	
 	
 	public static final Elemental[] fireElementals = new Elemental[256];
 	
-	public static final Elemental imp = new Elemental(FIRE, 0, 0.5f);
+	public static final Elemental imp = new Elemental(FIRE, 0, 0.5f, 0, 0, 0);
+	
+	public static final Elemental fire = new Elemental(FIRE, 253, 0.0f, 0, 0, 0);
+	
 	
 	public static final Elemental[] waterElementals = new Elemental[256];
 	
-	public static final Elemental sprayfish = new Elemental(WATER, 0, 0.2f);
+	public static final Elemental sprayfish = new Elemental(WATER, 0, 0.2f, 0, 0, 0);
+	
 	
 	public static final Elemental[] iceElementals = new Elemental[256];
 	public static final Elemental[] cloudElementals = new Elemental[256];
+	public static final Elemental[] magmaElementals = new Elemental[256];
 	
 	/*
-	 * Air and Earth combined would unleash plagues and gravel storms.
-	 * Air and Water would generate gales and hurricanes.
-	 * Fire and Earth would raise volcanoes and magma giants.
-	 * Fire and Water 
+	 * Air and Earth combined would unleash plagues and materialized death. Fast-moving
+	 * 		area-controlling elementals.
+	 * Air and Water would generate gales and hurricanes. Massive area-controlling elementals.
+	 * Fire and Earth would raise volcanoes and magma giants. Tanking elementals and localized
+	 * 		total control.
+	 * A delicate combining of Fire and Water would have the potential to create life.
+	 * 		Fast surgical area-control.
 	 */
 	
-	public Elemental(ECastType type, int i, float size) {
+	int range;
+	
+	public Elemental(ECastType type, int i, float size, int health, int attack, int speed) {
+		super(health, attack);
+		range = 0;
+		
+		this.entity = new ElementalEntity(this, size);
 		
 		switch (type) {
 		case AIR: 
@@ -51,15 +68,24 @@ public class Elemental extends Base {
 		case CLOUD:
 			cloudElementals[i] = this;
 			break;
+		case MAGMA:
+			magmaElementals[i] = this;
+			break;
 		default:
-//			throw new NullElementException();
 		}
-		
-		this.entity = new ElementalEntity(this, size);
+	}
+	
+	protected Elemental range(int r) {
+		this.range = r;
+		return this;
 	}
 	
 	public void update() {
 		//The elemental should either carry out instructions or follow default AI;
+	}
+
+	public void update(long dtime) {
+		//Not necessarily necessary.
 	}
 	
 }
