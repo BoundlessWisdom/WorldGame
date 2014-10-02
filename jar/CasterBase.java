dcf2af35cc254662be48e3a727037a9f60e092a7
Albert Wen <wisdomboundless@gmail.com>
2014-10-02 14:18:05 -0400

    Update CasterBase.java

--- jar/CasterBase.java
+++ jar/CasterBase.java
@@ -3,10 +3,28 @@ package jar;
 import java.util.HashMap;
 
 public abstract class CasterBase extends Base {
+
+	float power = 1.0f;
+	
+	//Update control bars: power * regenspeed * regenscale * update time in ?
+	
+	public CasterBase(float mh, int attack) {
+		super(mh, attack);
+	}
+	
+	public void updatePower() {
+		if (health/maxHealth >= 0.8f) {
+			power = 1.0f;
+			return;
+		}
+		power = (float) (1.0f - 1.0f/Math.pow(80, 4) * Math.pow(0.8f - (health/maxHealth), 4));
+	}
+	
+	/*****************************************************/
 	
 	HashMap<String, Boolean> control = new HashMap<String, Boolean>();
 	
-	boolean canCastControl(String attempt) {
+	public boolean canCast(String attempt) {
 		return control.get(attempt);
 	}
 
@@ -15,6 +33,11 @@ public abstract class CasterBase extends Base {
 	}
 	
 	public Location target;
+	public Location[] targets;
+	
 	public Elemental summoningElemental;
 	public Elemental[] summoningElementalGroup;
+	
+	public Projectile launchingProjectile;
+	public Projectile[] launchingProjectiles;
 }
