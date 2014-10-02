3f4827412eb8052fa7efed9637c2d9e17f8aad8f
Albert Wen <wisdomboundless@gmail.com>
2014-10-02 14:19:45 -0400

    Update FreeEntity.java

--- jar/FreeEntity.java
+++ jar/FreeEntity.java
@@ -1,9 +1,14 @@
 package jar;
 
-public class FreeEntity {
+public abstract class FreeEntity extends Entity {
+
+	FreeEntity(Base b, float size) {
+		super(b, size);
+	}
 	
 	/*
 	 * This is a placeholder for flying objects and anything else that temporarily leaves the coordinate system.
 	 */
 	
+	protected abstract void onLanding();
 }
