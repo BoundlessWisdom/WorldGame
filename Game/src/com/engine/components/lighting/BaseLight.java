c33f90f55dc3da8b3a29c6c94d360ebcf7bcb8f4
mallyar <rmallyas@hotmail.com>
2014-10-08 13:46:53 -0400

    BLAH

---
+++ Game/src/com/engine/components/lighting/BaseLight.java
@@ -0,0 +1,52 @@
+package com.engine.components.lighting;
+
+import com.engine.components.GameComponent;
+import com.engine.core.Vector3f;
+import com.engine.rendering.RenderingEngine;
+import com.engine.rendering.shaders.Shader;
+
+public class BaseLight extends GameComponent 
+{
+	private Vector3f color;
+	private float intensity;
+	private Shader shader;
+	
+	public BaseLight(Vector3f color, float intensity)
+	{
+		this.color = color;
+		this.intensity = intensity;
+	}
+	
+	public void setShader(Shader shader) 
+	{
+		this.shader = shader;
+	}
+	
+	public Shader getShader() 
+	{
+		return shader;
+	}
+
+	public Vector3f getColor() {
+		return color;
+	}
+
+	public void setColor(Vector3f color) {
+		this.color = color;
+	}
+
+	public float getIntensity() {
+		return intensity;
+	}
+
+	public void setIntensity(float intensity) {
+		this.intensity = intensity;
+	}
+	
+	@Override
+	public void addToRenderingEngine(RenderingEngine renderingEngine) 
+	{
+		super.addToRenderingEngine(renderingEngine);
+		renderingEngine.addLight(this);
+	}
+}
