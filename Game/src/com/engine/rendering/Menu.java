b5de2008389744426ad010bb8aec27e7d489bad2
briankilcullen <kilcullenb@gmail.com>
2014-10-22 13:42:15 -0400

    Clicks detected, must find a way to separate from main game.

--- Game/src/com/engine/rendering/Menu.java
+++ Game/src/com/engine/rendering/Menu.java
@@ -1,13 +1,20 @@
 package com.engine.rendering;
+
+
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+
+import com.engine.core.Input;
 //TODO: Maybe find a better place for this file
+import com.game.OurGame;
 
 public class Menu // Actions triggered by mouse clicks
 {
 	public Menu() // Creates menu and displays
 	{
 		
-		
-		
+		// Display menu here, probably a 2-D overlay still image
 		Update();
 	}
 	
@@ -23,10 +30,22 @@ public class Menu // Actions triggered by mouse clicks
 	
 	public void Update() // Handles mouse clicks
 	{
-		while(true)
-		{
-			
-		}
+			Input.update();
+			if(Input.getMousePosition() != null)
+			{
+				System.out.println(Input.getMousePosition().getX());
+				System.out.println(Input.getMousePosition().getY());
+				System.out.println(OurGame.state);
+			}
+			if((Input.getMousePosition().getX() < Display.getWidth()/2) && (Input.getMousePosition().getY() < Display.getHeight()/2))
+			{
+				System.out.println("Hovering over button!");
+				if(Input.getMouse(1)) // Menu setup currently runs on right-clicks in order to avoid meddling with mouse-centering system.
+				{
+					OurGame.state = true;
+					System.out.printf("Clicking!\n");
+				}
+			}
 	}
 	
 	public void Quit() // For people too lazy to click the X in the top corner
