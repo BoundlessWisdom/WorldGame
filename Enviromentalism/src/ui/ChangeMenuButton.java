999f50c7605ef5eef896a31882f5cf48a6c74e2b
briankilcullen <kilcullenb@gmail.com>
2014-11-05 13:36:52 -0500

    Upload what I have of menu so that Rohan can help.

--- Enviromentalism/src/ui/ChangeMenuButton.java
+++ Enviromentalism/src/ui/ChangeMenuButton.java
@@ -6,8 +6,8 @@ public class ChangeMenuButton extends Button {
 
 	private Menu changeTo;
 	
-	public ChangeMenuButton(Menu menu, int index, int x, int xlength, int y, int ylength) {
-		super(menu, index, x, xlength, y, ylength);
+	public ChangeMenuButton(Menu menu, int x, int xlength, int y, int ylength) {
+		super(menu, x, xlength, y, ylength);
 	}
 	
 	public Button setVariable(Object par) {
