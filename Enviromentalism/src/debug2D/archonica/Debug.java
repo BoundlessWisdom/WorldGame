8ea84352e08b463448e735bc7ab6eed4706723d9
briankilcullen <kilcullenb@gmail.com>
2014-11-19 14:08:19 -0500

    Prepared simulated world initialization.

--- Enviromentalism/src/debug2D/archonica/Debug.java
+++ Enviromentalism/src/debug2D/archonica/Debug.java
@@ -2,14 +2,34 @@ package debug2D.archonica;
 
 public class Debug {
 	
-	Location[][] worldArray;
+	static Location[][] worldArray;
 	
 	//Borders
 	//Tiles
 	//Print tiles?
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{
+		for(int i = 0; i < worldArray.length; i++)
+		{
+			for(int j = 0; j < worldArray[i].length; j++)
+			{
+				if((i % 2) == 0 || i == 0)
+				{
+					if(j == 0 || (j % 2) == 0)
+						worldArray[i][j] = new Point(i, j);
+					else
+						worldArray[i][j] = new Interaction(i, j);
+				}
+				else
+				{
+					if(j == 0 || (j % 2) == 0)
+						worldArray[i][j] = new Interaction(i, j);
+					else
+						worldArray[i][j] = new Tile(i, j);
+				}
+			}
+		}
 	}
 	
 	public void print() {
