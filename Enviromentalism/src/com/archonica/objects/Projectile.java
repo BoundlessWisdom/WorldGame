de2377dc136832b86a306752f0092e00b77a42b7
nsimon <nsimon@MH201-1221.ucvts.tec.nj.us>
2015-01-14 14:51:22 -0500

    fix error in includes

--- Enviromentalism/src/com/archonica/objects/Projectile.java
+++ Enviromentalism/src/com/archonica/objects/Projectile.java
@@ -2,7 +2,9 @@ package com.archonica.objects;
 
 import com.archonica.Updateable;
 import com.archonica.sparks.SparkProjectile;
+import com.engine.core.GameObject;
 import com.engine.core.Vector2f;
+import com.engine.core.Vector3f;
 import com.game.EntityObject;
 
 public abstract class Projectile extends EntityObject implements Updateable {
